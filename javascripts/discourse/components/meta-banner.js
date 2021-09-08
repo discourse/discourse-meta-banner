d3e28d6e2d43bb2475cd7691da9ff20afb2fdbfc
Justin DiRose <justin@justindirose.com>
2021-09-08 13:34:02 -0500

    Initial commit

---
+++ javascripts/discourse/components/meta-banner.js
@@ -0,0 +1,17 @@
+import Component from "@ember/component";
+import discourseComputed from "discourse-common/utils/decorators";
+import { inject as service } from "@ember/service";
+
+export default Component.extend({
+  router: service(),
+
+  @discourseComputed(
+    "currentUser"
+  )
+  shouldShow(currentUser) {
+    const isStaff = currentUser && currentUser.staff;
+    const lowTrustLevel = currentUser && currentUser.trust_level < 2;
+    // show banner only for anons and < TL 2
+    return !isStaff && (!currentUser || lowTrustLevel);
+  },
+});
