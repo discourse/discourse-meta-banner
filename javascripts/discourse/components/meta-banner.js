77a21ef929496ec81ea38f97596d9bf00964bcff
awesomerobot <kris.aubuchon@discourse.org>
2021-09-10 12:18:25 -0400

    UX: Update styles, shorten text, limit routes

--- javascripts/discourse/components/meta-banner.js
+++ javascripts/discourse/components/meta-banner.js
@@ -3,13 +3,18 @@ import discourseComputed from "discourse-common/utils/decorators";
 import { inject as service } from "@ember/service";
 
 export default Component.extend({
-  @discourseComputed(
-    "currentUser"
-  )
+  router: service(),
+
+  @discourseComputed("currentUser")
   shouldShow(currentUser) {
     const isStaff = currentUser && currentUser.staff;
     const lowTrustLevel = currentUser && currentUser.trust_level < 2;
     // show banner only for anons and < TL 2
     return !isStaff && (!currentUser || lowTrustLevel);
+  },
+
+  @discourseComputed("router.currentRouteName", "router.currentURL")
+  discoveryRoute(currentRouteName, currentURL) {
+    return currentRouteName.indexOf("discovery") > -1;
   },
 });
