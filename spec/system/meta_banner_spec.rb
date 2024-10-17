b4f2e322df57137e9132c0b23d840ec7f456fabc
Kelv <kelv@discourse.org>
2024-10-17 17:27:25 +0800

    DEV: Update linting setup (#13)

--- spec/system/meta_banner_spec.rb
+++ spec/system/meta_banner_spec.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-
 RSpec.describe "Meta Banner", type: :system do
   let!(:theme) { upload_theme_component }
   fab!(:user) { Fabricate(:user, trust_level: TrustLevel[3]) }
@@ -8,17 +7,13 @@ RSpec.describe "Meta Banner", type: :system do
   it "displays meta banner on the homepage" do
     visit("/")
 
-    expect(page).to have_css(
-      ".custom-banner-wrapper",
-    )
+    expect(page).to have_css(".custom-banner-wrapper")
   end
 
   it "does not display banner on routes other than the homepage" do
     visit("/top")
 
-    expect(page).not_to have_css(
-      ".custom-banner-wrapper",
-    )
+    expect(page).not_to have_css(".custom-banner-wrapper")
   end
 
   it "does not display for high trust users" do
@@ -26,8 +21,6 @@ RSpec.describe "Meta Banner", type: :system do
 
     visit("/")
 
-    expect(page).not_to have_css(
-      ".custom-banner-wrapper",
-    )
+    expect(page).not_to have_css(".custom-banner-wrapper")
   end
 end
